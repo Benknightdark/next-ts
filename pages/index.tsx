7a4df73e2a0ef2ba11271f910c7d379831da4540
moon791017 <moon791017@gmail.com>
2020-03-12 17:08:55 +0800

    OK

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,20 +2,21 @@ import { NextPage } from 'next';
 import Link from 'next/link';
 import Header from '../components/Header';
 import Layout from '../components/MyLayout';
-
+const PostLink = (props:any) => (
+  <li>
+    <Link href={`/post?title=${props.title}`}>
+      <a>{props.title}</a>
+    </Link>
+  </li>
+);
 const Home: NextPage<{ userAgent: string }> = ({ userAgent }) => (
-
-    // <div>
-
-    //     <div>
-    //         <Header />
-    //         <p>Hello Next.js</p>
-    //         <h1>Hello world! - user agent: {userAgent}</h1>
-
-    //     </div>
-    // </div>
     <Layout>
     <h1>Hello world! - user agent: {userAgent}</h1>
+    <ul>
+        <PostLink title="Hello Next.js" />
+        <PostLink title="Learn Next.js is awesome" />
+        <PostLink title="Deploy apps with Zeit" />
+      </ul>
   </Layout>
 );
 
@@ -25,3 +26,4 @@ Home.getInitialProps = async ({ req }) => {
 };
 
 export default Home;
+
