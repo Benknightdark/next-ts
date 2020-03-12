450eb6c71b8ead66e8e8ec1c81ac46d713f98465
moon791017 <moon791017@gmail.com>
2020-03-12 17:18:39 +0800

    add fucking amazing dynamic route

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,27 +2,27 @@ import { NextPage } from 'next';
 import Link from 'next/link';
 import Header from '../components/Header';
 import Layout from '../components/MyLayout';
-const PostLink = (props:any) => (
+const PostLink = (props: any) => (
   <li>
-    <Link href={`/post?title=${props.title}`}>
-      <a>{props.title}</a>
+    <Link href="/p/[id]" as={`/p/${props.id}`}>
+      <a>{props.id}</a>
     </Link>
   </li>
 );
 const Home: NextPage<{ userAgent: string }> = ({ userAgent }) => (
-    <Layout>
+  <Layout>
     <h1>Hello world! - user agent: {userAgent}</h1>
     <ul>
-        <PostLink title="Hello Next.js" />
-        <PostLink title="Learn Next.js is awesome" />
-        <PostLink title="Deploy apps with Zeit" />
-      </ul>
+      <PostLink id="hello-nextjs" />
+      <PostLink id="learn-nextjs" />
+      <PostLink id="deploy-nextjs" />
+    </ul>
   </Layout>
 );
 
 Home.getInitialProps = async ({ req }) => {
-    const userAgent = req ? req.headers['user-agent'] || '' : navigator.userAgent;
-    return { userAgent };
+  const userAgent = req ? req.headers['user-agent'] || '' : navigator.userAgent;
+  return { userAgent };
 };
 
 export default Home;
