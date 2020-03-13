a7ec65933bdc45e8ef719d3cb00c9871a4724fa5
moon791017 <moon791017@gmail.com>
2020-03-13 10:32:37 +0800

    OK

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import fetch from 'isomorphic-unfetch';
 import Layout from '../components/MyLayout';
 import { PostLinkModel } from '../models/PostLinkModel';
 import { IndexPageModel } from '../models/IndexPageModel';
+import { useEffect } from 'react';
 const PostLink = (props: PostLinkModel) => (
   <li>
     <Link href="/p/[id]" as={`/p/${props.id}`}>
@@ -11,27 +12,65 @@ const PostLink = (props: PostLinkModel) => (
     </Link>
   </li>
 );
-const Home: NextPage<IndexPageModel> = (prop:IndexPageModel) => (
-  <Layout>
-    <h1>Hello world! - user agent:{prop.userAgent}    </h1>
-    <div>{prop.data.length}</div>
-    <ul>
-      <PostLink id="hello-nextjs" />
-      <PostLink id="learn-nextjs" />
-      <PostLink id="deploy-nextjs" />
-    </ul>
-  </Layout>
-);
+const Home: NextPage<IndexPageModel> = (props: IndexPageModel) => {
+  useEffect(() => {
+    document.title = 'Home';
+  });
+  return (
+    <Layout>
+    <style jsx>{`
+        h1,
+        a {
+          font-family: 'Arial';
+        }
+
+        ul {
+          padding: 0;
+        }
+
+        li {
+          list-style: none;
+          margin: 5px 0;
+        }
+
+        a {
+          text-decoration: none;
+          color: blue;
+        }
+
+        a:hover {
+          opacity: 0.6;
+        }
+      `}</style>
+      <h1>Hello world! - user agent:{props.userAgent}    </h1>
+      <ul>
+        <PostLink id="hello-nextjs" />
+        <PostLink id="learn-nextjs" />
+        <PostLink id="deploy-nextjs" />
+      </ul>
+      <ul>
+        {props.data.map(show => (
+          <li key={show.id}>
+            <Link href="/p/[id]" as={`/p/${show.id}`}>
+              <a>{show.name}</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </Layout>
+  )
+};
 
 Home.getInitialProps = async ({ req }) => {
+
   const userAgent = req ? req.headers['user-agent'] || '' : navigator.userAgent;
   const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
   const data = await res.json();
 
   console.log(`Show data fetched. Count: ${data.length}`)
-  let prop=new IndexPageModel();
-  prop.userAgent=userAgent;
-  prop.data=data;
+  let prop = new IndexPageModel();
+  prop.userAgent = userAgent;
+  prop.data = data.map((entry: any) => entry.show);
   return prop;
 };
 
